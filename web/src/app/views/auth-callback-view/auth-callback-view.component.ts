7591d30de4db0033bcfd4a021493a4f6e872d8c3
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-07-25 14:25:30 -0600

    feat: show loader while authenticating

--- web/src/app/views/auth-callback-view/auth-callback-view.component.ts
+++ web/src/app/views/auth-callback-view/auth-callback-view.component.ts
@@ -3,27 +3,31 @@ import { Component, inject, PLATFORM_ID } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "@auth0/auth0-angular";
 import { filter } from "rxjs";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
   selector: "app-auth-callback-view",
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   templateUrl: "./auth-callback-view.component.html",
   styleUrl: "./auth-callback-view.component.scss"
 })
 export class AuthCallbackViewComponent {
   private platformId = inject(PLATFORM_ID);
   private router = inject(Router);
+  authService?: AuthService;
 
   constructor() {
     if (isPlatformBrowser(this.platformId)) {
-      const authService = inject(AuthService);
-      authService.isAuthenticated$.pipe(filter((authenticated) => authenticated)).subscribe(() => {
-        const location = localStorage.getItem("location");
-        if (location) this.router.navigateByUrl(location);
-        else this.router.navigateByUrl("/");
-        localStorage.removeItem("location");
-      });
+      this.authService = inject(AuthService);
+      this.authService.isAuthenticated$
+        .pipe(filter((authenticated) => authenticated))
+        .subscribe(() => {
+          const location = localStorage.getItem("location");
+          if (location) this.router.navigateByUrl(location);
+          else this.router.navigateByUrl("/");
+          localStorage.removeItem("location");
+        });
     }
   }
 }
