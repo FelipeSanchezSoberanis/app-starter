5fb91e5a9a3c1363a55e1250a4d07e53e4e4b88d
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-07-26 08:19:53 -0600

    feat: enable method authorization

--- api/src/main/java/com/felipe/starter/config/SecurityConfig.java
+++ api/src/main/java/com/felipe/starter/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.felipe.starter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
--- api/src/main/java/com/felipe/starter/controllers/UserController.java
+++ api/src/main/java/com/felipe/starter/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.felipe.starter.controllers;
 
 import com.felipe.starter.responses.UserDetailsResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   @GetMapping("/me")
+  @PreAuthorize("hasAuthority('SCOPE_read:current_user')")
   public ResponseEntity<UserDetailsResponse> getLoggedInUserDetails(
       @AuthenticationPrincipal Jwt jwt) {
     return ResponseEntity.ok(
